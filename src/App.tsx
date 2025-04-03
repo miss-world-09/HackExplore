8a8de259d34738175f88f6f01854621cfb454a33
Disha <disha.sahay2005@gmail.com>
2025-04-04 00:51:50 +0530

    added app

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import TeamFormation from "./pages/TeamFormation";
+import Internships from "./pages/Internships";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/internships" element={<Internships />} />
+          <Route path="/team-formation" element={<TeamFormation />} />
+          <Route path="/team-formation/:hackathonId" element={<TeamFormation />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
